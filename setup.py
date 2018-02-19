febe3f12c16cfda99be0bf6fa06915052054db13
Aldarien <aldarien85@gmail.com>
2018-02-19 15:05:44 -0300

    Changed name

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='pyconfig',
+setup(name='aconfig',
       version='1.0.0',
-      description='Config Files Manager',
-      url='http://github.com/aldarien/pyconfig',
+      description='Config Files Manager for JSON and YAML files',
+      url='http://github.com/aldarien/aconfig',
       author='Aldarien',
       author_email='aldarien85@gmail.com',
       license='MIT',
       packages=['pyconfig'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=[
+          "pyyaml>=3.0, <4.0"
+          ])
